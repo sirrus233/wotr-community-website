cd6927d88f8c0b25ee625b3eb92245cff9494b3a
Bradley Sherman <sirrus233@gmail.com>
2024-12-10 21:06:11 -0800

    Toy webserver

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,8 +1,18 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Data.Aeson (ToJSON)
+import Network.Wai.Handler.Warp (run)
+import Servant.API (Get, JSON, type (:>))
+import Servant.Server (serve)
+
+data User = User {name :: String, age :: Int, email :: String} deriving (Generic)
+
+type UserAPI = "users" :> Get '[JSON] User
+
+instance ToJSON User
+
+userAPI :: Proxy UserAPI
+userAPI = Proxy
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = run 8081 $ serve userAPI (pure $ User {name = "Gandalf", age = 6000, email = "mithrandir@valinor.net"})
