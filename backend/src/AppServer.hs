89bc620d5eccbe6e44c0e842dea689647193ef9b
Bradley Sherman <sirrus233@gmail.com>
2024-12-16 18:40:38 -0800

    Transaction support (#22)

--- backend/src/AppServer.hs
+++ backend/src/AppServer.hs
@@ -1,14 +1,16 @@
 module AppServer where
 
 import Api (Api)
+import Data.Pool (withResource)
 import Data.Time (UTCTime)
 import Data.Time.Clock (getCurrentTime)
 import Data.Validation (Validation (..))
 import Database (insertGameReport, insertPlayerIfNotExists)
+import Database.SQLite.Simple (withTransaction)
 import Servant (ServerError (errBody), ServerT, throwError)
 import Servant.Server (err422)
 import Types.Api (GameReport (..))
-import Types.App (AppM)
+import Types.App (AppM, Env (..))
 import Types.DataField (PlayerId)
 import Types.Database (ReadProcessedGameReport (..), WriteProcessedGameReport (..))
 import Validation (validateReport)
@@ -44,10 +46,12 @@ submitReportHandler :: GameReport -> AppM ReadProcessedGameReport
 submitReportHandler r = case validateReport r of
   Failure errors -> throwError $ err422 {errBody = show errors}
   Success report -> do
-    winnerId <- insertPlayerIfNotExists report.winner
-    loserId <- insertPlayerIfNotExists report.loser
-    now <- liftIO getCurrentTime
-    insertGameReport $ processReport now winnerId loserId report
+    env <- ask
+    liftIO . withResource env.dbPool $ \conn -> withTransaction conn $ do
+      winnerId <- insertPlayerIfNotExists conn report.winner
+      loserId <- insertPlayerIfNotExists conn report.loser
+      now <- liftIO getCurrentTime
+      insertGameReport conn $ processReport now winnerId loserId report
 
 server :: ServerT Api AppM
 server = submitReportHandler
