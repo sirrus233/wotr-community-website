d33b16a116730d503cefda85fef44ee235bd9590
Bradley Sherman <sirrus233@gmail.com>
2024-12-25 19:20:33 -0800

    Why not re-write the server (#30)

--- backend/src/Database.hs
+++ backend/src/Database.hs
@@ -1,157 +1,79 @@
 module Database where
 
-import Control.Exception (throwIO)
-import Data.Pool (withResource)
-import Database.SQLite.Simple (Only (..), execute_, query, query_)
-import Database.SQLite.Simple qualified as SQL
-import Types.App (Env (..), log)
-import Types.DataField (PlayerId, PlayerName, Rating, Side)
+import Data.Time (Year)
+import Database.Esqueleto.Experimental
+  ( Entity (..),
+    Key,
+    SqlPersistT,
+    desc,
+    from,
+    orderBy,
+    table,
+    val,
+    where_,
+    (==.),
+    (^.),
+  )
+import Database.Esqueleto.Experimental qualified as SQL
+import Types.DataField (PlayerName)
 import Types.Database
-  ( ReadPlayer (..),
-    ReadProcessedGameReport (..),
-    WritePlayer (..),
-    WriteProcessedGameReport (..),
-    WriteRatingChange (..),
+  ( EntityField (..),
+    GameReport,
+    Key (..),
+    Player (..),
+    PlayerId,
+    PlayerStats (..),
+    RatingDiff,
+    Unique (..),
+    currentYear,
+    defaultPlayerStats,
+    rolloverPlayerStats,
   )
 
-data UnexpectedResultException = UnexpectedResultException deriving (Show)
+getPlayerByName :: (MonadIO m) => PlayerName -> SqlPersistT m (Maybe (Entity Player))
+getPlayerByName name = SQL.getBy $ UniquePlayerName name
 
-instance Exception UnexpectedResultException
-
-readZeroOrOne :: IO [r] -> IO (Maybe r)
-readZeroOrOne rs = rs >>= \case [] -> pure Nothing; [r] -> pure $ Just r; _ -> throwIO UnexpectedResultException
-
-readOne :: IO [r] -> IO r
-readOne rs = rs >>= \case [r] -> pure r; _ -> throwIO UnexpectedResultException
-
-initializeDatabase :: ReaderT Env IO ()
-initializeDatabase = do
-  env <- ask
-  liftIO . withResource env.dbPool $ \conn -> do
-    log env.logger "Initializing database"
-    tableCount <- fromOnly <$> readOne (query_ conn "SELECT COUNT(*) FROM sqlite_master WHERE type='table';") :: IO Int
-    case tableCount of
-      0 -> do
-        log env.logger "Creating tables"
-        execute_ conn "PRAGMA foreign_keys = ON;"
-        execute_
-          conn
-          "CREATE TABLE GameReports (\
-          \id INTEGER NOT NULL,\
-          \timestamp INTEGER NOT NULL,\
-          \winnerId INTEGER NOT NULL,\
-          \loserId INTEGER NOT NULL,\
-          \side TEXT NOT NULL,\
-          \victory TEXT NOT NULL,\
-          \match TEXT NOT NULL,\
-          \competition TEXT,\
-          \league TEXT,\
-          \expansions TEXT NOT NULL,\
-          \treebeard INTEGER,\
-          \actionTokens INTEGER NOT NULL,\
-          \dwarvenRings INTEGER NOT NULL,\
-          \turns INTEGER NOT NULL,\
-          \corruption INTEGER NOT NULL,\
-          \mordor INTEGER,\
-          \initialEyes INTEGER NOT NULL,\
-          \aragornTurn INTEGER,\
-          \strongholds TEXT NOT NULL,\
-          \interestRating INTEGER NOT NULL,\
-          \comments TEXT,\
-          \PRIMARY KEY(id),\
-          \FOREIGN KEY(winnerId) REFERENCES Players(id),\
-          \FOREIGN KEY(loserId) REFERENCES Players(id)\
-          \);"
-        execute_
-          conn
-          "CREATE TABLE Players (\
-          \id INTEGER NOT NULL,\
-          \name TEXT NOT NULL UNIQUE,\
-          \country TEXT,\
-          \PRIMARY KEY(id)\
-          \);"
-        execute_
-          conn
-          "CREATE TABLE Ratings (\
-          \id INTEGER NOT NULL,\
-          \playerId INTEGER NOT NULL,\
-          \side TEXT NOT NULL,\
-          \timestamp INTEGER NOT NULL,\
-          \reportId INTEGER NOT NULL,\
-          \ratingBefore INTEGER NOT NULL,\
-          \ratingAfter INTEGER NOT NULL,\
-          \PRIMARY KEY(id),\
-          \FOREIGN KEY(playerId) REFERENCES Players(id),\
-          \FOREIGN KEY(reportId) REFERENCES GameReports(id));"
-        execute_ conn "CREATE INDEX idx_reports_timestamp ON GameReports (timestamp DESC);"
-        execute_ conn "CREATE INDEX idx_players_name ON Players (name);"
-        execute_ conn "CREATE INDEX idx_ratings_player ON Ratings (playerId, side, timestamp DESC);"
-      _ -> log env.logger "Database already initialized"
-
-getPlayerByName :: SQL.Connection -> PlayerName -> IO (Maybe ReadPlayer)
-getPlayerByName conn name = readZeroOrOne $ query conn "SELECT * FROM Players WHERE name = ?" (Only name)
-
-insertPlayerIfNotExists :: SQL.Connection -> PlayerName -> IO PlayerId
-insertPlayerIfNotExists conn name = do
-  player <- getPlayerByName conn name
-  case player of
+insertPlayerIfNotExists :: (MonadIO m) => PlayerName -> SqlPersistT m (Key Player)
+insertPlayerIfNotExists name =
+  getPlayerByName name >>= \case
+    Just (Entity playerKey _) -> pure playerKey
     Nothing -> do
-      let insertPlayerQuery = "INSERT INTO Players (name, country) VALUES (?, ?) RETURNING id"
-      fromOnly <$> readOne (query conn insertPlayerQuery (WritePlayer {name, country = Nothing}))
-    Just (ReadPlayer {pid}) -> pure pid
+      playerKey <- SQL.insert $ Player name Nothing
+      year <- currentYear
+      SQL.insert_ $ defaultPlayerStats playerKey year
+      pure playerKey
 
-getLatestRating :: SQL.Connection -> Rating -> PlayerId -> Side -> IO Rating
-getLatestRating conn defaultRating pid side = do
-  let getRatingQuery = "SELECT ratingAfter FROM Ratings WHERE (playerId, side) = (?, ?) ORDER BY timestamp DESC LIMIT 1"
-  rating <- fmap fromOnly <$> readZeroOrOne (query conn getRatingQuery (pid, side))
-  case rating of
-    Nothing -> pure defaultRating
-    Just r -> pure r
+getStats :: (MonadIO m) => PlayerId -> Year -> SqlPersistT m PlayerStats
+getStats pid year =
+  SQL.get (PlayerStatsKey pid (fromIntegral year)) >>= \case
+    Just stats -> pure stats
+    Nothing -> do
+      priorStats <- getMostRecentStats pid
+      let stats = rolloverPlayerStats pid year priorStats
+      SQL.insert_ stats
+      pure stats
 
-insertRatingChange :: SQL.Connection -> WriteRatingChange -> IO Rating
-insertRatingChange conn rating = do
-  let insertRatingQuery =
-        "INSERT INTO Ratings (\
-        \playerId,\
-        \side,\
-        \timestamp,\
-        \reportId,\
-        \ratingBefore,\
-        \ratingAfter\
-        \) VALUES (?, ?, ?, ?, ?, ?)\
-        \RETURNING ratingAfter"
-  fromOnly <$> readOne (query conn insertRatingQuery rating)
+getMostRecentStats :: (MonadIO m) => PlayerId -> SqlPersistT m PlayerStats
+getMostRecentStats pid =
+  recentStats >>= \case
+    Just (Entity _ stats) -> pure stats
+    Nothing -> do
+      year <- currentYear
+      let stats = defaultPlayerStats pid year
+      SQL.insert_ stats
+      pure stats
+  where
+    recentStats = SQL.selectOne $ do
+      stats <- from $ table @PlayerStats
+      where_ (stats ^. PlayerStatsPlayerId ==. val pid)
+      orderBy [desc (stats ^. PlayerStatsYear)]
+      pure stats
 
-insertGameReport :: SQL.Connection -> WriteProcessedGameReport -> IO ReadProcessedGameReport
-insertGameReport conn report@(WriteProcessedGameReport {..}) = do
-  let insertReportQuery =
-        "INSERT INTO GameReports (\
-        \timestamp,\
-        \winnerId,\
-        \loserId,\
-        \side,\
-        \victory,\
-        \match,\
-        \competition,\
-        \league,\
-        \expansions,\
-        \treebeard,\
-        \actionTokens,\
-        \dwarvenRings,\
-        \turns,\
-        \corruption,\
-        \mordor,\
-        \initialEyes,\
-        \aragornTurn,\
-        \strongholds,\
-        \interestRating,\
-        \comments\
-        \) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)\
-        \RETURNING id"
-  rid <- fromOnly <$> readOne (query conn insertReportQuery report)
+replacePlayerStats :: (MonadIO m) => PlayerStats -> SqlPersistT m ()
+replacePlayerStats stats@(PlayerStats {..}) = SQL.replace (PlayerStatsKey playerStatsPlayerId playerStatsYear) stats
 
-  let nameQuery = "SELECT name FROM Players WHERE id = ?"
-  winner <- fromOnly <$> readOne (query conn nameQuery (Only winnerId))
-  loser <- fromOnly <$> readOne (query conn nameQuery (Only loserId))
+insertRatingChange :: (MonadIO m) => RatingDiff -> SqlPersistT m ()
+insertRatingChange = SQL.insert_
 
-  pure ReadProcessedGameReport {..}
+insertGameReport :: (MonadIO m) => GameReport -> SqlPersistT m (Key GameReport)
+insertGameReport = SQL.insert
--- backend/src/Types/App.hs
+++ backend/src/Types/App.hs
@@ -1,17 +1,21 @@
 module Types.App where
 
-import Data.Pool (Pool)
+import Database.Esqueleto.Experimental (SqlPersistT)
+import Database.Esqueleto.Experimental qualified as SQL
 import Database.Redis qualified as Redis
-import Database.SQLite.Simple qualified as SQL
 import Servant (Handler)
 import System.Log.FastLogger (LogStr, TimedFastLogger, ToLogStr (..))
 
-data Env = Env {dbPool :: Pool SQL.Connection, redisPool :: Redis.Connection, logger :: TimedFastLogger}
+data Env = Env {dbPool :: SQL.ConnectionPool, redisPool :: Redis.Connection, logger :: TimedFastLogger}
 
 type AppM = ReaderT Env Handler
 
 nt :: Env -> AppM a -> Handler a
-nt s x = runReaderT x s
+nt env server = runReaderT server env
 
+runDb :: SqlPersistT IO a -> AppM a
+runDb dbAction = asks dbPool >>= liftIO . SQL.runSqlPool dbAction
+
+-- TODO Run in handler w/o explicit logger pass?
 log :: TimedFastLogger -> LogStr -> IO ()
 log logger msg = logger (\time -> toLogStr time <> " " <> msg <> "\n")
