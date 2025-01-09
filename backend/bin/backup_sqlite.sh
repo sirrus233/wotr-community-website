cc79ef25e6b8f31cbea0b33dc0a9659943c8faaa
Bradley Sherman <sirrus233@gmail.com>
2025-01-08 22:19:11 -0800

    Create bucket and fix some script issues

--- backend/bin/backup_sqlite.sh
+++ backend/bin/backup_sqlite.sh
@@ -4,11 +4,14 @@ set -euo pipefail
 
 DB_PATH=$1
 DB_BACKUP_BUCKET=$2
+YEAR=$(date +"%Y")
+MONTH=$(date +"%m")
+DAY=$(date +"%d")
 TIMESTAMP=$(date +"%Y-%m-%dT%H:%M:%SZ")
 TMP_BACKUP="/tmp/db-backup-$TIMESTAMP.sqlite"
 
 sqlite3 "$DB_PATH" ".backup '$TMP_BACKUP'"
 gzip "$TMP_BACKUP"
 TMP_BACKUP_ZIP="${TMP_BACKUP}.gz"
-aws s3 cp "$TMP_BACKUP" "${DB_BACKUP_BUCKET}/${TIMESTAMP}.sqlite"
-rm "$TMP_BACKUP"
+aws s3 cp "$TMP_BACKUP_ZIP" "s3://${DB_BACKUP_BUCKET}/${YEAR}/${MONTH}/${DAY}/${TIMESTAMP}.sqlite.gz"
+rm "$TMP_BACKUP" "$TMP_BACKUP_ZIP"
