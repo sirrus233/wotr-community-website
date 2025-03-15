0bbefd2b26d9ad802968cebcaca7a07fd106424c
Bradley Sherman <sirrus233@gmail.com>
2025-03-15 01:01:36 -0700

    Reattach volume

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -118,11 +118,11 @@ export class InfrastructureStack extends cdk.Stack {
             removalPolicy: cdk.RemovalPolicy.RETAIN,
         });
 
-        // new ec2.CfnVolumeAttachment(this, "AttachServerStorage", {
-        //     volumeId: ebsVolume.volumeId,
-        //     instanceId: instance.instanceId,
-        //     device: "/dev/sda2",
-        // });
+        new ec2.CfnVolumeAttachment(this, "AttachServerStorage", {
+            volumeId: ebsVolume.volumeId,
+            instanceId: instance.instanceId,
+            device: "/dev/sda2",
+        });
 
         instance.connections.allowFrom(
             ec2.Peer.ipv4("97.113.5.72/32"),
