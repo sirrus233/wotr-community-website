fdffa331bcc8065b6693246488c7c22b9a188b22
Chelsea Schmidt <63372749+chelseaSchmidt@users.noreply.github.com>
2025-09-14 12:54:02 -0700

    Support frontend boolean filtering and add game log filter (#298)

---
+++ frontend/src/Table/styledComponents.tsx
@@ -0,0 +1,12 @@
+import { styled } from "@mui/joy/styles";
+
+export const FilterContainer = styled("div")({
+    boxSizing: "border-box",
+    display: "flex",
+    alignItems: "end",
+    justifyContent: "center",
+    height: "100%",
+    padding: "0 5px",
+    fontWeight: "normal",
+    lineHeight: "1em",
+});
