3bf6b0b4b48a3ff5dad89491165899faf0a95c6f
Chelsea Schmidt <63372749+chelseaSchmidt@users.noreply.github.com>
2025-10-31 12:15:15 -0700

    Frontend components directory (#305)

---
+++ frontend/src/components/Leagues.tsx
@@ -0,0 +1,279 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Box from "@mui/joy/Box";
+import IconButton from "@mui/joy/IconButton";
+import Modal from "@mui/joy/Modal";
+import ModalClose from "@mui/joy/ModalClose";
+import ModalDialog from "@mui/joy/ModalDialog";
+import { expansionLeagues, leagueTiers, LEAGUE_START_YEAR } from "../constants";
+import { RefreshRequest } from "../hooks/useRequestState";
+import {
+    HEADER_HEIGHT_PX,
+    HEADER_MARGIN_PX,
+    TABLE_BTN_HEIGHT_PX,
+    TABLE_ELEMENTS_GAP,
+    BUTTON_SELECTOR_HEIGHT,
+} from "../styles/sizes";
+import { LeagueParams, LeagueStats } from "../types";
+import {
+    getLeagueLabel,
+    getLeagueTierLabel,
+    isDefined,
+    noNansense,
+    range,
+    toPercent,
+} from "../utils";
+import ButtonSelector from "./ButtonSelector";
+import LeaguePlayerForm from "./LeaguePlayerForm";
+import Table from "./Table";
+import { ColHeaderData, RowData } from "./Table/types";
+import TableLayout from "./TableLayout";
+
+const TABLE_TOP_POSITION =
+    HEADER_HEIGHT_PX +
+    HEADER_MARGIN_PX +
+    BUTTON_SELECTOR_HEIGHT * 2 +
+    TABLE_BTN_HEIGHT_PX +
+    TABLE_ELEMENTS_GAP * 4;
+
+interface Props {
+    stats: LeagueStats;
+    playerNames: string[];
+    loading: boolean;
+    loadingPlayers: boolean;
+    isAdmin: boolean;
+    error: string | null;
+    params: LeagueParams;
+    setParams: React.Dispatch<React.SetStateAction<LeagueParams>>;
+    refresh: RefreshRequest;
+}
+
+export default function Leagues({
+    stats,
+    playerNames,
+    loading,
+    loadingPlayers,
+    isAdmin,
+    error,
+    params,
+    setParams,
+    refresh,
+}: Props) {
+    const [leaguePlayerFormOpen, setLeaguePlayerFormOpen] = useState(false);
+
+    const availableYears = range(
+        LEAGUE_START_YEAR,
+        new Date().getFullYear() + 1
+    );
+
+    return (
+        <Box>
+            {leaguePlayerFormOpen && (
+                <Modal open onClose={() => setLeaguePlayerFormOpen(false)}>
+                    <ModalDialog maxWidth="200px">
+                        <ModalClose />
+
+                        <LeaguePlayerForm
+                            league={params.league}
+                            tier={params.tier}
+                            year={params.year}
+                            playerNames={playerNames}
+                            loadingPlayers={loadingPlayers}
+                            refresh={refresh}
+                        />
+                    </ModalDialog>
+                </Modal>
+            )}
+
+            <ButtonSelector
+                current={params.year}
+                options={availableYears.reverse()}
+                setCurrent={(year) =>
+                    setParams((params) => ({ ...params, year }))
+                }
+                style={{ marginBottom: `${TABLE_ELEMENTS_GAP}px` }}
+            />
+
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                overflow="auto"
+            >
+                <Box display="flex" alignItems="center" mr={1} fontSize="sm">
+                    Base:
+                    <ButtonSelector
+                        current={
+                            params.league === "GeneralLeague"
+                                ? params.tier
+                                : null
+                        }
+                        options={leagueTiers.slice()}
+                        setCurrent={(tier) =>
+                            setParams((params) => ({
+                                ...params,
+                                league: "GeneralLeague",
+                                tier,
+                            }))
+                        }
+                        getLabel={getLeagueTierLabel}
+                        variant="outlined"
+                        selectedVariant="solid"
+                        style={{ marginLeft: "5px" }}
+                    />
+                </Box>
+
+                <Box display="flex" alignItems="center" fontSize="sm">
+                    Expansions:
+                    <ButtonSelector
+                        current={params.league}
+                        options={expansionLeagues.slice()}
+                        setCurrent={(league) =>
+                            setParams((params) => ({
+                                ...params,
+                                league,
+                                tier: "Tier1",
+                            }))
+                        }
+                        getLabel={getLeagueLabel}
+                        variant="outlined"
+                        selectedVariant="solid"
+                        style={{ marginLeft: "5px" }}
+                    />
+                </Box>
+            </Box>
+
+            <TableLayout
+                loading={loading}
+                error={error}
+                refresh={refresh}
+                containerStyle={{
+                    maxHeight: `calc(100vh - ${TABLE_TOP_POSITION}px - ${TABLE_ELEMENTS_GAP}px)`,
+                }}
+                table={
+                    <LeagueTable
+                        stats={stats}
+                        loading={loading}
+                        isAdmin={isAdmin}
+                        openLeaguePlayerForm={() =>
+                            setLeaguePlayerFormOpen(true)
+                        }
+                    />
+                }
+            />
+        </Box>
+    );
+}
+
+interface LeagueTableProps {
+    stats: LeagueStats;
+    loading: boolean;
+    isAdmin: boolean;
+    openLeaguePlayerForm: () => void;
+}
+
+function LeagueTable({
+    stats,
+    loading,
+    isAdmin,
+    openLeaguePlayerForm,
+}: LeagueTableProps) {
+    const entries = Object.entries(stats).sort(
+        ([, playerStatsA], [, playerStatsB]) =>
+            playerStatsB.summary.points - playerStatsA.summary.points
+    );
+
+    const FIXED_HEADERS = ["Win Rate", "Games", "Wins", "Points"];
+
+    return (
+        <Table
+            cornerHeaders={[
+                {
+                    key: "player",
+                    width: 145,
+                    content: isAdmin ? (
+                        <IconButton
+                            size="sm"
+                            disabled={loading}
+                            onClick={openLeaguePlayerForm}
+                            variant="solid"
+                            color="primary"
+                        >
+                            <AddIcon />
+                        </IconButton>
+                    ) : (
+                        ""
+                    ),
+                },
+            ].filter(isDefined)}
+            colHeaders={[
+                ...FIXED_HEADERS.map(
+                    (headerLabel): ColHeaderData => ({
+                        key: headerLabel,
+                    })
+                ),
+                ...entries.map(
+                    ([playerId, playerStats]): ColHeaderData => ({
+                        key: playerId,
+                        content: playerStats.name,
+                    })
+                ),
+            ]}
+            rows={entries.map(
+                ([playerId, playerStats]): RowData => ({
+                    key: playerId,
+                    cells: [
+                        { key: playerStats.name },
+                        {
+                            key: `${playerId}-winRate`,
+                            content: toPercent(
+                                noNansense(
+                                    playerStats.summary.totalWins /
+                                        playerStats.summary.totalGames
+                                )
+                            ),
+                        },
+                        {
+                            key: `${playerId}-totalGames`,
+                            content: playerStats.summary.totalGames,
+                        },
+                        {
+                            key: `${playerId}-wins`,
+                            content: playerStats.summary.totalWins,
+                        },
+                        {
+                            key: `${playerId}-points`,
+                            content: playerStats.summary.points.toFixed(1),
+                        },
+                        ...entries.map(([opponentId]) => {
+                            const gameStatsForOpponent =
+                                playerStats.gameStatsByOpponent[
+                                    Number(opponentId)
+                                ];
+                            return {
+                                key: `${playerId}-${opponentId}`,
+                                content: (
+                                    <Box
+                                        sx={
+                                            playerId === opponentId
+                                                ? {
+                                                      background: "#ccc",
+                                                      color: "#ccc",
+                                                      borderRadius: "5px",
+                                                  }
+                                                : {}
+                                        }
+                                    >
+                                        {gameStatsForOpponent
+                                            ? `${gameStatsForOpponent.wins}-${gameStatsForOpponent.losses}`
+                                            : "-"}
+                                    </Box>
+                                ),
+                            };
+                        }),
+                    ],
+                })
+            )}
+        />
+    );
+}
---
+++ frontend/src/components/PlayerRemapForm.tsx
@@ -0,0 +1,130 @@
+import axios from "axios";
+import React, { useState } from "react";
+import Alert from "@mui/joy/Alert";
+import {
+    MenuOption,
+    PlayerRemapFormData,
+    ValidPlayerRemapFormData,
+} from "../types";
+import AdminFormLayout from "./AdminFormLayout";
+import Autocomplete from "./Autocomplete";
+import useConditionalActionEffect from "../hooks/useConditionalActionEffect";
+import useFormData, { initializeToDefaults } from "../hooks/useFormData";
+import { ErrorMessage } from "../constants";
+import { API_BASE_URL } from "../env";
+import { toErrorMessage } from "../networkErrorHandlers";
+
+interface Props {
+    pid: number;
+    name: string;
+    playerOptions: MenuOption<number>[];
+    refresh: () => void;
+}
+
+export default function PlayerRemapForm({
+    pid,
+    name,
+    playerOptions,
+    refresh,
+}: Props) {
+    const initialFormData: PlayerRemapFormData = {
+        fromPlayer: initializeToDefaults({ id: pid, label: name }),
+        toPlayer: {
+            value: null,
+            error: null,
+            validate: function _() {
+                return this.value
+                    ? pid === this.value.id
+                        ? "Cannot remap player to themselves"
+                        : null
+                    : null;
+            },
+        },
+    };
+
+    const [
+        formData,
+        { errorOnSubmit, successMessage, submitting },
+        { handleInputChange, validateField, handleSubmit },
+    ] = useFormData<PlayerRemapFormData, ValidPlayerRemapFormData>({
+        initialFormData,
+        optionalFields: [],
+        missingFieldErrorMessage: ErrorMessage.ExistingPlayerRequired,
+        submit,
+        toErrorMessage,
+    });
+
+    const [warningAlert, setWarningAlert] = useState<string | null>(null);
+
+    useConditionalActionEffect(!!successMessage, refresh);
+
+    const buttonText = "Remap player";
+
+    return (
+        <>
+            <AdminFormLayout
+                header={name}
+                submitting={submitting}
+                errorOnSubmit={errorOnSubmit}
+                buttonText={buttonText}
+                successMessage={successMessage}
+                shouldHideFormOnSuccess
+                formElementsProps={[
+                    {
+                        label: "Reassign this player's games to:",
+                        error: formData.toPlayer.error,
+                        element: (
+                            <Autocomplete
+                                current={formData.toPlayer.value}
+                                options={playerOptions}
+                                placeholder="Player name"
+                                loading={false}
+                                onChange={handleInputChange("toPlayer")}
+                                validate={validateField("toPlayer")}
+                            />
+                        ),
+                    },
+                ]}
+                handleSubmit={() => {
+                    if (warningAlert || formData.toPlayer.error) {
+                        handleSubmit();
+                    } else {
+                        setWarningAlert(
+                            `Danger: ${name} will cease to exist. ${formData.toPlayer.value?.label} will absorb all of ${name}'s history. ${name}'s history cannot be recovered. If you're sure, press "${buttonText}" again to continue.`
+                        );
+                    }
+                }}
+            />
+
+            {!successMessage && warningAlert && (
+                <Alert color="warning" sx={{ my: "10px" }}>
+                    {warningAlert}
+                </Alert>
+            )}
+        </>
+    );
+}
+
+async function submit(validFormData: ValidPlayerRemapFormData) {
+    return await axios.post(
+        `${API_BASE_URL}/remapPlayer`,
+        toPayload(validFormData),
+        {
+            headers: { "Content-Type": "application/json" },
+        }
+    );
+}
+
+type PlayerRemapPayload = {
+    fromPid: number;
+    toPid: number;
+};
+
+function toPayload(
+    validFormData: ValidPlayerRemapFormData
+): PlayerRemapPayload {
+    return {
+        fromPid: validFormData.fromPlayer.value.id,
+        toPid: validFormData.toPlayer.value.id,
+    };
+}
