bb20708fff394558232864996c9d5fdf9bb28bee
Bradley Sherman <sirrus233@gmail.com>
2026-02-18 02:21:45 -0800

    comms

--- backend/src/Types/TypeScriptInstances.hs
+++ backend/src/Types/TypeScriptInstances.hs
@@ -7,15 +7,12 @@ import Data.Time (UTCTime)
 import Servant (NoContent (..))
 import Servant.Multipart (FileData, Tmp)
 
--- | UTCTime encodes to JSON as an ISO8601 string, so mirror that for TypeScript.
 instance TypeScript UTCTime where
   getTypeScriptType _ = "string"
 
--- | Treat multipart file uploads as browser File objects for generated TS.
 instance TypeScript (FileData Tmp) where
   getTypeScriptType _ = "File"
 
--- | Map Ordering to the FE union alias defined manually in TS.
 instance TypeScript Ordering where
   getTypeScriptType _ = "ApiInequalityOperator"
   getTypeScriptDeclarations _ =
@@ -23,6 +20,5 @@ instance TypeScript Ordering where
         "export type ApiInequalityOperator = \"GT\" | \"LT\" | \"EQ\";"
     ]
 
--- | Encode Servant's NoContent as void in TypeScript.
 instance TypeScript NoContent where
   getTypeScriptType _ = "void"
