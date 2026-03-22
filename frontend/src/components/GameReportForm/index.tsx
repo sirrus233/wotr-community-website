4f9d3541f7dcc09488541703c8460839244ef0b6
Bradley Sherman <sirrus233@gmail.com>
2026-03-21 18:06:56 -0700

    colon

--- frontend/src/components/GameReportForm/index.tsx
+++ frontend/src/components/GameReportForm/index.tsx
@@ -757,9 +757,9 @@ function validationErrorToMessage(
         case "InvalidStronghold":
             return "invalid stronghold selections for the indicated expansions";
         case "InvalidRotkAragorn":
-            return "when playing RotK, Aragorn must be crowned on turn 1";
+            return "when playing RotK: Aragorn must be crowned on turn 1";
         case "InvalidRotkEyes":
-            return "when playing RotK, Shadow must allocate 3 eyes on turn 1";
+            return "when playing RotK: Shadow must allocate 3 eyes on turn 1";
     }
 }
 
